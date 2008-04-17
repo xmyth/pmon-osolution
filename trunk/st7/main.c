5f975f571f90e55cbd5e8231353cbe25833ff7cd
swadmin.os <swadmin.os@6178231c-4f3a-0410-920f-eb57272d133b>
2008-04-17 12:59:17 +0000

    add rtc interrupt

--- trunk/st7/main.c
+++ trunk/st7/main.c
@@ -3,8 +3,30 @@
  * Copyright (c) 2002-2005 STMicroelectronics
  */
 
+#include "ST7FLI49MK1T6.h"
+#include "common.h"
+
+void sys_init(void);
+
+unsigned char lt2_rtc_count = 0;
 
 main()
 {
+	sys_init();
+	EnableInterrupts;
 	while (1);
+}
+
+/*
+ * interrupt 
+ */
+ 
+@interrupt void LT2_RTC_INT(void) {
+	lt2_rtc_count++;
+	LTCSR1;
+}
+
+
+void sys_init(void) {
+	LTCSR1 = 0x30;
 }
