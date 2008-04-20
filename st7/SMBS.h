327b83f93e499043ddb1ec3eee6170ad3a310073
swadmin.os <swadmin.os@6178231c-4f3a-0410-920f-eb57272d133b>
2008-04-20 06:23:49 +0000

    Add I2C Support

---
+++ st7/SMBS.h
@@ -0,0 +1,72 @@
+/*********************************Documentation**********************************
+General Purpose - This file declares all the hardware registers as extern
+
+********************************Revision History*******************************
+_______________________________________________________________________________
+Date :18/07/2003                  Release:1.0       	   		        	      
+******************************************************************************/
+#ifndef SMBS_HR_H
+#define SMBS_HR_H 
+
+void SMBS_Init (unsigned char Slave_Add1, unsigned char Slave_Add2);
+
+#include "ST7FLI49MK1T6.h"
+/*****************************************************************************/
+/***** P E R I P H E R A L S   R E G I S T E R S   D E F I N I T I O N S *****/
+/*****************************************************************************/
+#define BIT0 1
+#define BIT1 2
+#define BIT2 4
+#define BIT3 8
+#define BIT4 0x10
+#define BIT5 0x20
+#define BIT6 0x40
+#define BIT7 0x80
+
+
+
+/*****************************************************************************/
+/***** H A R D W A R E   R E G I S T E R   B I T   D E F I N I T I O N S *****/
+/*****************************************************************************/
+
+//CCR
+#define ITE         BIT0                           /* Interrupt enable flags */  
+#define STOP        BIT1                   /* Stop condition indication flag */
+#define ACK         BIT2                             /* I2C acknowledge flag */
+#define START				BIT3		
+#define ENGC				BIT4										/* Enable General Call bit */
+#define PE          BIT5                           /* Peripheral enable flag */
+
+//CSR1
+#define	SB					BIT0
+#define	MSMODE			BIT1
+#define ADSL        BIT2                 /* Address matched indication flage */
+#define BTF         BIT3                        /* Byte transfer status flag */
+#define BUSY				BIT4
+#define TRA         BIT5                           /* Interrupt enable flags */
+#define ADD10				BIT6
+#define	EVF					BIT7
+
+//CSR2
+#define GCAL				BIT0
+#define BERR        BIT1
+#define ARLO				BIT2 
+#define STOPF       BIT3
+#define AF          BIT4
+
+
+//OAR2
+#define FRI0        BIT6                               /* I2COAR2 FR[i] bits */
+
+#define EVT_NONE	  0
+#define EVT1				1
+#define EVT2				2
+#define EVT3				3
+#define EVT3_1			31
+#define EVT4				4
+
+#define Fcpu ((unsigned long) 8000000)   
+
+#endif
+
+/*** (c) 2003   STMicroelectronics ****************** END OF FILE ************/
